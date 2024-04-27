6b6873768075e3b57d52f100faaba61a05628675
im-beltagy <imbeltagy@gmail.com>
2024-04-27 04:52:32 +0300

    trying to fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    rollupOptions: {
+      external: [
+        "/src/common/SectionTitle/Index",
+        "/src/assets/sass/resets",
+        "/src/assets/sass/variables",
+        "/src/assets/sass/configs",
+      ],
+    },
+  },
+});
